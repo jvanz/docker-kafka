ab30bcf195df586e3c693dfd290872545b39cbab
José Guilherme Vanz <guilherme.sft@gmail.com>
2016-05-15 22:24:41 -0300

    Initial commit
    Added first version of the Dockerfile and script to start Kafka server

---
+++ start_kafka.sh
@@ -0,0 +1,5 @@
+
+sed -i.bak s/zookeeper.connect=localhost:2181/zookeeper.connect=$ZOOKEEPER_PORT_2181_TCP_ADDR:2181/g $KAFKA_HOME/config/server.properties
+
+cd $KAFKA_HOME
+bin/kafka-server-start.sh config/server.properties
